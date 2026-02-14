82cd5e89edfc1e89626de3452ae23540f91e2168
Shokirjon <samandarovsakir@gmail.com>
2026-02-14 13:53:51 +0500

    Functioinni tahlil qilish

---
+++ lesson - 37/app.js
@@ -0,0 +1,9 @@
+// functtionni tahlil qilish
+// Tahlil
+
+function calcAge(birthYear,firsName) {
+	const age = 2026 - birthYear
+	console.log(`${firsName}ning yoshi ${age}`);
+	return age
+}
+const newAge = calcAge(2000,'Shokirjon')
