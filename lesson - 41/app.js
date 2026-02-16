fbdcfef7c54b1ba6e97ddce6280b0d7ebc9a8228
Shokirjon <samandarovsakir@gmail.com>
2026-02-16 21:52:06 +0500

    object bilan ishlash

--- lesson - 41/app.js
+++ lesson - 41/app.js
@@ -1,4 +1,30 @@
-function sayHello() {
-	console.log('Sodda function JS da')
-}
-sayHello()
+// object
+const users = {
+	name: 'shokirjon',
+	firsName: 'samandarov',
+	age: 24,
+	email: 'samandarovshakirjon@gmail.com',
+	location: 'Xorazm',
+	langs: ['rus','eng','uzb'],
+	login: function() {
+		console.log('Siz sahifangizga kirdingiz.')
+	},
+	logut: function() {
+		console.log('Siz sahifangizdan chiqib kettingiz!')
+	},
+	speaks: function() {
+
+		}
+	}
+
+// console.log(users.name)
+// console.log(users.firsName)
+// console.log(users['age'])
+users.login() // object ichidagi login functionni chaqiradi.
+users.logut()
+
+// nimaga nuqta orqali ismni chaqirib toUpperCase() methodidan foydalanyabmiz!
+// chunki JS da hamma narsa bundan korinib turibdiki object hisoblanadi
+const ism = 'Doniyor'
+ism.toUpperCase()
+
