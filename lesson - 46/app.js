5eb8477ad0feec4056c05702364d51474e69f949
Shokirjon <samandarovsakir@gmail.com>
2026-02-17 23:18:04 +0500

    DOM Selector

---
+++ lesson - 46/app.js
@@ -0,0 +1,27 @@
+// Dom selectors
+// getElementByTagName
+// const listIteam = document.getElementsByTagName('li')
+// console.log(listIteam.length)
+// listIteam.forEach(li => {
+// 	console.log(li)
+// });
+// HTMLCollection da forEach methodi ishlamaydi chunki uni array ko`rishida yozmaydi
+
+
+// getElementByClassName
+// const listIteam = document.getElementsByClassName('.list-iteam')
+// console.log(listIteam)
+
+// getElementById
+// const listIteam = document.getElementById('click-btn')
+// console.log(listIteam)
+
+// 	querySelector
+// const listIteam = document.querySelector('button')
+// console.log(listIteam)
+
+// const listIteam = document.querySelectorAll('.list-iteam')
+// listIteam.forEach((iteam) => {
+// 	console.log(iteam)
+})
+// 	querySelectorAll
