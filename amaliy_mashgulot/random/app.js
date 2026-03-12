86849ca98f22ffe8d1a96d25d080bdac31699471
zokirovich <samandarovshakirjon@gmail.com>
2026-03-12 11:51:15 +0500

    new_project_home_work

---
+++ amaliy_mashgulot/random/app.js
@@ -0,0 +1,8 @@
+// btn 
+const btnRoll = document.querySelector('.btn--roll')
+const btnNew = document.querySelector('.btn--new')
+const btnHodl = document.querySelector('.btn--hold')
+
+// dice image
+const diceImg = document.querySelector('.dice')
+
