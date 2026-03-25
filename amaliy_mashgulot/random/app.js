793766574338aa359d8ed3c2e9f2dc3cd17bba07
Shokir <87795605+zokirovich@users.noreply.github.com>
2026-03-25 13:46:17 +0500

    Delete amaliy_mashgulot/random directory

--- amaliy_mashgulot/random/app.js
+++
@@ -1,8 +0,0 @@
-// btn 
-const btnRoll = document.querySelector('.btn--roll')
-const btnNew = document.querySelector('.btn--new')
-const btnHodl = document.querySelector('.btn--hold')
-
-// dice image
-const diceImg = document.querySelector('.dice')
-
